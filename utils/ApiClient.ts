b25f7834b06b97cb5e6339f5b6c08b4106ad13eb
Mohammed Musthafa <musthafa.m1996@gmail.com>
2024-12-28 04:10:30 +0530

    fix: envs

--- utils/ApiClient.ts
+++ utils/ApiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios client with a dynamic base URL from environment variable
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5003", // Default to local if not set
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Default to local if not set
   headers: {
     "Content-Type": "application/json",
   },
